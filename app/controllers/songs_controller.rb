64284111c392815e1c39529072fcacc4aba59f85
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-09 19:36:31 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -16,8 +16,8 @@ class SongsController < ApplicationController
     if Song.exists?(params[:id])
       @song = Song.find(params[:id])
       if @song.artist_name == ""
-        @songs = Song.all
-        render :index, alert: "Add Artist"
+        #@songs = Song.all
+        #render :index, alert: "Add Artist"
         #redirect_to artist_songs_path(@song.artist.id), alert: "Add Artist"
       end
     else
