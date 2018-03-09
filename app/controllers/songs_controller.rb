d810667089bda4a7687d47ca779da05753eec427
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-09 17:10:12 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,9 +14,7 @@ class SongsController < ApplicationController
   def show
     if Song.exists?(params[:id])
       @song = Song.find(params[:id])
-      if !@song.artist
-        redirect_to edit_song_path, alert: "Add Artist."
-      end
+      redirect_to edit_song_path, alert: "Add Artist." unless @song.artist
     else
       redirect_to artist_songs_path(params[:artist_id]), alert: "Song not found."
     end
